63ded26c888678e3dfde025c526e72eee0bf1c42
koboko34 <66181314+koboko34@users.noreply.github.com>
2023-07-26 22:13:16 +0100

    Solved 2 problems in Java

---
+++ Java/Remainder.java
@@ -0,0 +1,13 @@
+
+public class Remainder {
+
+	public static void main(String[] args) {
+		System.out.println(remainder(1, 3));
+
+	}
+	
+	public static int remainder(int first, int second) {
+		return first % second;
+	}
+
+}
---
+++ Java/ReturnSomethingToMe.java
@@ -0,0 +1,13 @@
+
+public class ReturnSomethingToMe {
+
+	public static void main(String[] args) {
+		System.out.println(giveMeSomething("is better than nothing!"));
+
+	}
+	
+	public static String giveMeSomething(String s) {
+		return "something " + s;
+	}
+
+}
